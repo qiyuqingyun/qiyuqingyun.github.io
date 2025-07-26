8de0aea42b9ddc2b1f05127ecc0259e86a8827b8
qiyuqingyun <qingyun_989@qq.com>
2025-07-27 00:09:17 +0800

    ga

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,5 @@
 import { defineNuxtConfig } from 'nuxt/config'
-
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-26',
   modules: [
     '@nuxt/content',
     '@nuxtjs/tailwindcss'
@@ -10,9 +8,6 @@ export default defineNuxtConfig({
     // https://content.nuxtjs.org/api/configuration
     markdown: {
       // https://content.nuxtjs.org/api/configuration#markdown
-    },
-    highlight: {
-      theme: 'github-dark'
     }
   },
   css: [
@@ -20,17 +15,5 @@ export default defineNuxtConfig({
   ],
   devtools: {
     enabled: true
-  },
-  // GitHub Pages 配置
-  app: {
-    // 根据环境设置基础URL，GitHub Pages需要使用仓库名称作为路径
-    baseURL: process.env.NODE_ENV === 'production' ? '/qiyuqingyun.github.io/' : '/',
-    buildAssetsDir: '/_nuxt/',
-    // 路由配置
-    router: {
-      options: {
-        strict: false
-      }
-    }
   }
 })
