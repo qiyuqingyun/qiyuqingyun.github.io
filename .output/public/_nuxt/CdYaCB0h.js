7e57476b1023ec5e5e5da9b8b71c8502e55d72c8
qiyuqingyun <qingyun_989@qq.com>
2025-07-27 01:21:00 +0800

    gai

---
+++ .output/public/_nuxt/CdYaCB0h.js
@@ -0,0 +1,1 @@
+import{_ as a}from"./1D_94szV.js";import{A as n,o as s,w as f,B as p}from"./BaedlOSu.js";const l={__name:"ProseA",props:{href:{type:String,default:""},target:{type:String,default:void 0,required:!1}},setup(e){const t=e;return(r,_)=>{const o=a;return s(),n(o,{href:t.href,target:t.target},{default:f(()=>[p(r.$slots,"default")]),_:3},8,["href","target"])}}};export{l as default};
