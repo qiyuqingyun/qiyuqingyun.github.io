b317f9d3fbe6da0eb739090e29517ce56d234f70
qiyuqingyun <qingyun_989@qq.com>
2025-07-27 00:13:26 +0800

    改

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,6 @@
 import { defineNuxtConfig } from 'nuxt/config'
 export default defineNuxtConfig({
+  compatibilityDate: '2025-07-26',
   modules: [
     '@nuxt/content',
     '@nuxtjs/tailwindcss'
@@ -8,6 +9,9 @@ export default defineNuxtConfig({
     // https://content.nuxtjs.org/api/configuration
     markdown: {
       // https://content.nuxtjs.org/api/configuration#markdown
+    },
+    highlight: {
+      theme: 'github-dark'
     }
   },
   css: [
@@ -15,5 +19,10 @@ export default defineNuxtConfig({
   ],
   devtools: {
     enabled: true
+  },
+  // GitHub Pages 配置 - 根据官方文档设置
+  app: {
+    baseURL: process.env.DEPLOY_ENV === 'GH_PAGES' ? '/qiyuqingyun.github.io/' : '/',
+    buildAssetsDir: '/_nuxt/'
   }
 })
